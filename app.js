21f7f903b2e12a99b6f87d3afc2a9128ba64cd23
Jeff Cook <jecook2692@gmail.com>
2020-07-14 13:25:34 -0400

    adds package-lock.json file

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chatRoomRouter = require("./routes/chatRoom");
 
 var app = express();
+const cors = require("cors");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
