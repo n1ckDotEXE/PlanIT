f6a382a897fd5fd29673c17dd7d73d6d65264865
Nicholas Nam <admin@nicholasnam.com>
2020-07-18 19:57:01 -0400

    Re-addied [ENV] and deleted concurrently from package.json

--- models/index.js
+++ models/index.js
@@ -5,12 +5,12 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 
-const config = require(__dirname + "/../config/config.js");
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config, {dialect: "postgres"});
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
