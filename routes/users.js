7034e77ee13d10ed0916d7c881a81ded0d622b69
Nicholas Nam <admin@nicholasnam.com>
2020-07-22 15:02:49 -0400

    Fixes logout issue when refreshed

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models'); 
+const db = require('../models');
 const bcrypt = require('bcrypt');
 
-router.post('/auth/register', (req,res,) =>{ 
+router.post('/auth/register', (req, res,) => {
   const name = req.body.name_input;
   const email = req.body.email_input;
   const password = req.body.password_input;
 
 
   bcrypt.hash(password, 10, (err, hash) => {
-    db.Users.create({   
-      name: name,  
-      email:email,
+    db.Users.create({
+      name: name,
+      email: email,
       password: hash,
-    }).then((user) => { 
+    }).then((user) => {
       req.session.user = user;
       res.status(201).json(user)
-    }); 
+    });
   });
 });
- 
-router.post('/auth/login', (req,res) => { 
+
+router.post('/auth/login', (req, res) => {
   const { email, password } = req.body;
 
-  db.Users.findOne({ where: { email } }) 
+  db.Users.findOne({ where: { email } })
     .then((user) => {
-      bcrypt.compare(password, user.password, (err, match) => { 
-        if (match) { 
-          req.session.user = user; 
+      bcrypt.compare(password, user.password, (err, match) => {
+        if (match) {
+          req.session.user = user;
           res.status(200).json(user)
-        } else { 
+        } else {
           res.status(401).json({
             error: 'password is incorrect!'
           });
@@ -53,28 +53,42 @@ router.get('/auth/logout', (req, res) => {
 
 router.get('/', (req, res) => {
   db.Users.findAll(
-   {
-     include: [db.Gardens]
-   } 
+    {
+      include: [db.Gardens]
+    }
   )
-  .then(users => {
-    res.status(200).json(users)
-  })
+    .then(users => {
+      res.status(200).json(users)
+    })
+})
+
+router.get('/current', (req, res) => {
+  if (!req.session.user) {
+    res.status(401).json("not authorized")
+  }
+  db.Users.findByPk(
+    req.session.user.id,
+    {
+      include: [db.Gardens],
+      order: [[db.Gardens, "createdAt", "DESC"]]
+    }
+  )
+    .then(users => {
+      res.status(200).json(users)
+    })
 })
 
 router.get('/:id', (req, res) => {
   db.Users.findByPk(
     req.params.id,
-   {
-     include: [db.Gardens],
-     order: [[db.Gardens, "createdAt", "DESC"]]
-   } 
+    {
+      include: [db.Gardens],
+      order: [[db.Gardens, "createdAt", "DESC"]]
+    }
   )
-  .then(users => {
-    res.status(200).json(users)
-  })
+    .then(users => {
+      res.status(200).json(users)
+    })
 })
-
-
 
 module.exports = router;
